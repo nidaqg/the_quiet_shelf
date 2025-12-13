88c5035b569022c0b0f3755ec26aad4dd68c0552
nidaqg <nidaqg@gmail.com>
2025-12-12 22:53:12 -0500

    separate css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./styles.css";
+import "./styles/base.css";
+import "./styles/header.css";
+import "./styles/footer.css";
+import "./styles/components.css";
+import "./styles/modal.css";
+import "./styles/library.css";
+import "./styles/daily.css";
+import "./styles/monthly.css";
+import "./styles/add-book.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
